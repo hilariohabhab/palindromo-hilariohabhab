56fee10cfc16224bae8fe5d7e918c59661c8eade
hilariohabhab <hilariohabhab@hotmail.com>
2023-09-11 13:36:33 -0300

    Entrega do projeto de laços

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-function isPalindrome(str){
-    /* Seu código aqui */
-}
+function isPalindrome(str) {
+  let reverseText = "";
+  str = str.toLowerCase().replaceAll(" ", "");
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseText += str[i];
+  }
+
+  if (str === reverseText) {
+    return true;
+  } else {
+    return false;
+  }
+}
+isPalindrome();
+
+function arrayMaxMin(arr) {
+  let min = Number.MAX_VALUE;
+  let max = Number.MIN_VALUE;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  const result = [min, max];
+  return result;
 }
